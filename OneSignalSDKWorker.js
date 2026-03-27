34707682200edbf6d36b8588a9c6d00fc572a8e0
Cousinho555 <pablocouso5@gmail.com>
2026-03-27 22:34:09 +0100

    Implement service worker caching for static assets

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,29 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+const VERSION = 'tol01-v5';
+
+const STATIC_CACHE = [
+  '/',
+  '/index.html',
+  '/logo.png',
+  '/equipo.jpg',
+  '/jugador1.jpg',
+  '/jugador2.jpg',
+  '/jugador3.jpg',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(VERSION)
+      .then(cache => cache.addAll(STATIC_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== VERSION).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
